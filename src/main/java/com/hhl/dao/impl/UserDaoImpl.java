abf5864924c67b92571991378e39255d2e77d967
hhl <bbyshp@126.com>
2014-08-20 22:28:49 +0800

    simple code daoimpl

--- src/main/java/com/hhl/dao/impl/UserDaoImpl.java
+++ src/main/java/com/hhl/dao/impl/UserDaoImpl.java
@@ -1,11 +1,8 @@
 package com.hhl.dao.impl;
-
 import com.hhl.dao.UserDao;
 import com.hhl.dao.mapper.UserModelMapper;
 import com.hhl.model.mybatis.UserModel;
 import com.hhl.model.mybatis.UserModelCriteria;
-import org.apache.ibatis.session.SqlSession;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
@@ -15,16 +12,14 @@ import java.util.List;
  * Created by hhl on 2014/8/16.
  */
 @Repository
-public class UserDaoImpl implements UserDao {
+public class UserDaoImpl  implements UserDao {
 
-    @Autowired
-    private SqlSessionTemplate sqlSessionTemplate;
+   @Autowired
+   private UserModelMapper userModelMapper;
 
     @Override
     public List<UserModel> getUserList() {
         try {
-            SqlSession sqlSession = sqlSessionTemplate.getSqlSessionFactory().openSession();
-            UserModelMapper userModelMapper = sqlSession.getMapper(UserModelMapper.class);
             UserModelCriteria userModelCriteria = new UserModelCriteria();
             userModelCriteria.createCriteria().andIdIsNotNull();
             return userModelMapper.selectByExample(userModelCriteria);
