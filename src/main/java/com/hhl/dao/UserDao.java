a18b17a8aad5275f0481f98c68aeb2c2f8910722
hhl <huanghanlin@ewt.cc>
2014-09-10 16:52:43 +0800

    fix

--- src/main/java/com/hhl/dao/UserDao.java
+++ src/main/java/com/hhl/dao/UserDao.java
@@ -1,13 +1,36 @@
 package com.hhl.dao;
-
+import com.hhl.dao.mapper.UserModelMapper;
 import com.hhl.model.mybatis.UserModel;
+import com.hhl.model.mybatis.UserModelCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 /**
  * Created by hhl on 2014/8/16.
  */
-public interface UserDao {
+@Repository
+public class UserDao
+{
 
-    List<UserModel> getUserList();
+   @Autowired
+   private UserModelMapper userModelMapper;
+
+    public UserModelMapper getUserModelMapper()
+    {
+        return userModelMapper;
+    }
+
+    public List<UserModel> getUserList() {
+        try {
+            UserModelCriteria userModelCriteria = new UserModelCriteria();
+            userModelCriteria.createCriteria().andIdIsNotNull();
+            return userModelMapper.selectByExample(userModelCriteria);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
 }
