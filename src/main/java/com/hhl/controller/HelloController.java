6f8322ccbcea9dc8e9b2ed256517f8530bfd66f5
hhl <huanghanlin@ewt.cc>
2014-09-11 13:23:29 +0800

    6

--- src/main/java/com/hhl/controller/HelloController.java
+++ src/main/java/com/hhl/controller/HelloController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HelloController {
 
 
-    @RequestMapping(value = "index")
+    @RequestMapping(value = "")
     public String index(ModelMap modelMap){
         modelMap.put("test","hellw");
         return "index";
